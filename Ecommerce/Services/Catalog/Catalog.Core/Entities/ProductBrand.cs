f3906c38764551e07321b11de35ef6502f5aa207
karra <karrasaresh@gmail.com>
2024-08-06 12:40:19 +0000

    BrandContestSeed is added

--- Ecommerce/Services/Catalog/Catalog.Core/Entities/ProductBrand.cs
+++ Ecommerce/Services/Catalog/Catalog.Core/Entities/ProductBrand.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MongoDB.Bson.Serialization.Attributes;
 
 namespace Catalog.Core.Entities
 {
-    public class ProductBrand
+    public class ProductBrand:BaseEntity
     {
+        [BsonElement("Name")]
+        public string Name { get; set; }
     }
 }
--- Ecommerce/Services/Catalog/Catalog.Core/Entities/ProductType.cs
+++ Ecommerce/Services/Catalog/Catalog.Core/Entities/ProductType.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MongoDB.Bson.Serialization.Attributes;
 
 namespace Catalog.Core.Entities
 {
-    public class ProductType
+    public class ProductType:BaseEntity
     {
+        [BsonElement("Name")]   
+        public string Name { get; set; }
     }
 }
